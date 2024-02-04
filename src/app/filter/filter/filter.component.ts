a38dbfce909c0316f865c2d474125da0a4069a0f
Jbar91 <jibarlucea91@gmail.com>
2024-02-04 12:58:38 +0100

    Remove unwanted component

--- src/app/filter/filter/filter.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'filter',
-  templateUrl: './filter.component.html',
-  styleUrl: './filter.component.sass',
-})
-export class FilterComponent {}
