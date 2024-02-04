e04ffe35a7d0931e917e19e0208196945f4b2954
Jbar91 <jibarlucea91@gmail.com>
2024-02-04 12:50:19 +0100

    Add new app title & remove unnecessary app prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesModule } from './heroes/heroes.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroesModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
-  title = 'w2m-heroes';
+  title = 'W2M Superheroes';
 }
